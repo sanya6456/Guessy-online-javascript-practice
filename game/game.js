e9d5c2255d24415b4a59e86306521c8445d510a9
sanya6456 <66361881+sanya6456@users.noreply.github.com>
2020-06-22 20:07:17 +0200

    Revert "update03"
    This reverts commit 4d6341e5978e41d2479e324ef741ebda29b33e49.

--- game/game.js
+++ game/game.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-<<<<<<< HEAD
 
     //Game types:
     // 1. car brands
     var carbrands=['Audi','Bmw','Maserati','McLaren','Ford'];
-    var animals=['Tigris','Ló','Medve','Aranyhal','Fóka'];
 
-
-=======
->>>>>>> parent of 913a732... update02
     //Ready, Steady, Go! function
     var counter=0;
     var timer;
@@ -24,51 +19,40 @@ $(document).ready(function(){
             $('#guessy').html('Steady!');
         } if(timer==3) {
             $('#guessy').html('Go!');
-<<<<<<< HEAD
         } if(timer==4){
 
 
-            //Carbrands Game time!!
+            //_____ready, steady, go function end!_______
+
+            //Game time!!
             $('#guessy').html(function(){
                 return carbrands[Math.floor(Math.random() * carbrands.length)];
             });
 
-=======
->>>>>>> parent of 913a732... update02
         }
         
     }
-    //_____ready, steady, go function end!_______
+
+    //disable buttons for 4s
+    var fewSeconds = 4;
+
+    $('.btn').prop('disabled', true);
+    setTimeout(function(){
+        $('.btn').prop('disabled', false);
+    }, fewSeconds*1000);
+    //__________________________
 
 
-    //Game time!!
 
-<<<<<<< HEAD
-    //if click button go to next question
-  
-
-    $('#car').click(function(){
+    if($('#car').click()){
         $('.btn').click(function(){
+
             $('#guessy').html(function(){
-                return carbrands[Math.floor(Math.random() * carbrands.length)];;
+                return carbrands[Math.floor(Math.random()*carbrands.length)];
             });
         });
-    });
+    }
 
-    $('#animal').click(function(){
-        $('.btn').click(function(){
-            $('#guessy').html(function(){
-                return animals[Math.floor(Math.random() * animals.length)];
-            });
-        });
-    });
-
-=======
->>>>>>> parent of 913a732... update02
-    
-
-    
-    //___________
 
 
 
